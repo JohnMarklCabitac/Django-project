7b4ee27f3465f9c3e3aec77c8392b479967d6fc0
JohnMarklCabitac <jmcabitac@gmail.com>
2022-11-14 11:16:06 +0800

    commit

---
+++ projectsite/baseball/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.admin import display
+from .models import Position, Person, Club, Play
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("description",)
+    search_fields = ("description",)
+
+@admin.register(Person)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("lastname","firstname","height","weight")
+    search_fields = ("lastname","firstname","height","weight")
+
+@admin.register(Club)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name","coach","dorm_latitude","dorm_longitude")
+    search_fields = ("name","coach","dorm_latitude","dorm_longitude")
+
+@admin.register(Play)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("player","team","string_no","pos")
+    search_fields = ("player","team","string_no","pos")
